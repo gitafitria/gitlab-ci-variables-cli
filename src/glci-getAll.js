efbf0f173497428a869b57d1b4e7dcabd89fea8d
Gita Fitria Ratnasari <gita.fitria@autobahn-security.com>
2024-04-30 19:00:11 +0700

    feat: add environment variable to override environment value

--- src/glci-getAll.js
+++ src/glci-getAll.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
-import program from 'commander';
-import { getConf } from './lib/utils';
-import { savePropertiesFile } from './lib/properties-file';
-import gitlabCI from './lib/gitlab-ci';
+import program from "commander";
+import { getConf } from "./lib/utils";
+import { savePropertiesFile } from "./lib/properties-file";
+import gitlabCI from "./lib/gitlab-ci";
 
 async function execute() {
   const conf = await getConf();
 
-  const handler = gitlabCI(conf.url, conf.token);
+  let handler;
+  if (conf.environment) {
+    handler = gitlabCI(conf.url, conf.token, conf.environment);
+  } else {
+    handler = gitlabCI(conf.url, conf.token);
+  }
   const resp = await handler.listVariables();
 
-  console.log('Downloaded variables from Gitlab CI.');
+  console.log("Downloaded variables from Gitlab CI.");
 
   savePropertiesFile(`${process.cwd()}/${conf.output}`, resp);
 
@@ -21,20 +26,20 @@ async function execute() {
 }
 
 program
-  .description('Read all key/value pairs from Gitlab API and save them to the specified output file')
-  .option(
-    '--url <url>',
-    'Your Gitlab project URL, e.g. https://gitlab.com/gitlab-org/gitlab-ce',
+  .description(
+    "Read all key/value pairs from Gitlab API and save them to the specified output file"
   )
   .option(
-    '--token <token>',
-    'Your Gitlab token',
+    "--url <url>",
+    "Your Gitlab project URL, e.g. https://gitlab.com/gitlab-org/gitlab-ce"
   )
+  .option("--token <token>", "Your Gitlab token")
   .option(
-    '--output <output>',
-    'The location to save the result to',
-    'gitlab.env.yml',
+    "--output <output>",
+    "The location to save the result to",
+    "gitlab.env.yml"
   )
+  .option("--environment <output>", "The location to save the result to")
   .parse(process.argv);
 
 execute(program);
